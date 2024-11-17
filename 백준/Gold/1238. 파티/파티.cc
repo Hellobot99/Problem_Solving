e0f3be8703d78c0818b6424a5143b8b7948f6c6e
Hellobot99 <162524438+Hellobot99@users.noreply.github.com>
2024-11-17 23:17:55 +0900

    [Gold III] Title: 파티, Time: 768 ms, Memory: 2296 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.cc
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include <climits>
+using namespace std;
+
+class Node {
+public:
+    int value;
+    Node(int value);
+    Node();
+};
+
+Node::Node() {
+    this->value = 0;
+}
+
+Node::Node(int value) {
+    this->value = value;
+}
+
+class Graph {
+private:
+    map<int, Node> nodes;
+    map<int, vector<pair<int, int>>> adjacencyList;
+
+public:
+    void addNode(int value);
+    void addEdge(int from, int to, int weight);
+    void display();
+    void DFS(int value, vector<int>& visited);
+    void BFS(int value, vector<int>& visited);
+    int dijkstra(int start,int end);
+};
+
+void Graph::addNode(int value) {
+    if (nodes.find(value) == nodes.end()) {
+        nodes[value] = Node(value);
+    }
+    else {
+        cout << "Node " << value << " already exists." << endl;
+    }
+}
+int x;
+void Graph::addEdge(int from, int to, int weight) {
+    adjacencyList[from].push_back({ to, weight });
+}
+map<int, int> distances2;
+int Graph::dijkstra(int start, int end) {
+    map<int, int> distances;
+    for (auto& node : nodes) {
+        distances[node.first] = INT_MAX;
+    }
+    distances[start] = 0;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    pq.push(make_pair(0, start));
+
+    while (!pq.empty()) {
+        int currentDistance = pq.top().first;
+        int currentNode = pq.top().second;
+        pq.pop();
+        if (currentDistance > distances[currentNode]) continue;
+
+        for (auto& neighbor : adjacencyList[currentNode]) {
+            int nextNode = neighbor.first;
+            int edgeWeight = neighbor.second;
+
+            if (distances[currentNode] + edgeWeight < distances[nextNode]) {
+                distances[nextNode] = distances[currentNode] + edgeWeight;
+                pq.push(make_pair(distances[nextNode], nextNode));
+            }
+        }
+    }
+
+    if (start == x)distances2 = distances;
+    return distances[end];
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    Graph g;
+    int result = 0;
+    int n, m, a, b, c, start, end;
+    cin >> n >> m >> x;
+    for (int i = 1; i < n + 1; i++) {
+        g.addNode(i);
+    }
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b >> c;
+        g.addEdge(a, b, c);
+    }
+    g.dijkstra(x,x);
+    for (int i = 1; i < n + 1; i++) {
+        a = g.dijkstra(i,x);
+        b = distances2[i];
+        if (result < a + b)result = a + b;
+    }
+    cout << result;
+
+    return 0;
+}
