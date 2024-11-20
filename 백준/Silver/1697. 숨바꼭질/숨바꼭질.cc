e26e667db5ffef86a82c9a90d8accba4ac63f6d0
Hellobot99 <162524438+Hellobot99@users.noreply.github.com>
2024-11-20 14:42:59 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2284 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include <climits>
+using namespace std;
+
+int solve(bool visited[], int n, int k) {
+    queue<pair<int, int>> q;
+    int count = 0;
+    q.push({ n, 0 });
+    visited[n] = true;
+    while (!q.empty()) {
+        int current = q.front().first;
+        int steps = q.front().second;
+        q.pop();
+        if (current == k) {
+            return steps;
+        }
+        
+        if (current * 2 < 100001 && !visited[current * 2]) {
+            visited[current * 2] = true;
+            q.push({ current * 2, steps + 1 });
+        }
+        if (current - 1 >= 0 && !visited[current - 1]) {
+            visited[current - 1] = true;
+            q.push({ current - 1, steps + 1 });
+        }
+        if (current + 1 < 100001 && !visited[current + 1]) {
+            visited[current + 1] = true;
+            q.push({ current + 1, steps + 1 });
+        }
+    }
+    return -1;
+}
+
+int main() {
+
+    bool* visited =new bool[100001];
+    for (int i = 0; i < 100001; i++) {
+        visited[i] = false;
+    }
+
+    int n, k;
+    cin >> n >> k;
+
+    cout << solve(visited, n, k);
+
+    return 0;
+}
