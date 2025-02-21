e3b47f2dc809edc9c74c55186ce99c75cebed103
Hellobot99 <162524438+Hellobot99@users.noreply.github.com>
2025-02-21 15:31:22 +0900

    [Gold V] Title: A와 B, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include <climits>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string S, T;
+    
+    cin >> S >> T;
+
+    while (S.size() < T.size()) {
+        if (T.back() == 'A') {
+            T.pop_back();
+        }
+        else {
+            T.pop_back();
+            reverse(T.begin(), T.end());
+        }
+    }
+
+    if (S == T) cout << 1;
+    else cout << 0;
+
+    return 0;
+}
